72a12238339fa4766a9daa1505daec3beef1b58d
ZhangJin <nbnbnb.zhang@gmail.com>
2017-02-11 18:22:14 +0800

    to 1.1 clean

--- ASPNETCoreApp/gulpfile.js
+++ ASPNETCoreApp/gulpfile.js
@@ -15,21 +15,14 @@ var paths = {
            'node_modules/rxjs/bundles/Rx.js']
 };
 
-// 依赖任务
 gulp.task('lib', function () {
-    // 将文件拷贝到 wwwroot/scripts/lib 目录中
     gulp.src(paths.libs).pipe(gulp.dest('wwwroot/scripts/lib'))
 });
 
-// 项目清理时执行
 gulp.task('clean', function () {
-    // 删除 wwwroot/scripts 中的所有文件
     return del(['wwwroot/scripts/**/*']);
 });
 
-// 项目生成后执行
 gulp.task('default', ['lib'], function () {
-    // 先执行 lib 任务
-    // 将项目 scripts 目录中的脚本文件拷贝到 wwwroot/scripts 目录中
     gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/scripts'))
 });
