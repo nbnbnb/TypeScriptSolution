eb8aeaa3acce92bef1d964dbbd8b03eded0ae25b
ZhangJin <nbnbnb.zhang@gmail.com>
2017-02-18 18:11:00 +0800

    format update

--- ASPNETWebApp/src/app.ts
+++ ASPNETWebApp/src/app.ts
@@ -1,5 +1,5 @@
-﻿import {Component} from "angular2/core"
-import {MyModel} from "./model"
+﻿import { Component } from "angular2/core"
+import { MyModel } from "./model"
 
 @Component({
     selector: `my-app`,
--- ASPNETWebApp/src/main.ts
+++ ASPNETWebApp/src/main.ts
@@ -1,3 +1,3 @@
-﻿import {bootstrap} from "angular2/platform/browser";
-import {MyApp} from "./app";
+﻿import { bootstrap } from "angular2/platform/browser";
+import { MyApp } from "./app";
 bootstrap(MyApp);
