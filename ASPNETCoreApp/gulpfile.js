686e373775e11641cd5b76904bebe2995dfa54c0
张进 <jinzhanga@Ctrip.com>
2017-02-23 15:25:05 +0800

    compile OK

--- ASPNETCoreApp/gulpfile.js
+++ ASPNETCoreApp/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='default' />
+﻿/// <binding AfterBuild='default' Clean='clean_scripts' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -8,31 +8,42 @@ var gulp = require('gulp');
 var del = require('del');
 
 var paths = {
-    scripts: ['scripts/**/*.js', 'scripts/**/*.ts', 'scripts/**/*.map'],
-    node_modules: 'node_modules/**/*'
+    scripts: "scripts/**/*",
+    libs: [
+        // angular js file
+        'node_modules/@angular/core/bundles/core.umd.js',
+        'node_modules/@angular/common/bundles/common.umd.js',
+        'node_modules/@angular/compiler/bundles/compiler.umd.js',
+        'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
+        'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        'node_modules/@angular/http/bundles/http.umd.js',
+        'node_modules/@angular/router/bundles/router.umd.js',
+        'node_modules/@angular/forms/bundles/forms.umd.js',
+
+        // npm normal libs
+        'node_modules/angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+        'node_modules/core-js/client/shim.min.js',
+        'node_modules/zone.js/dist/zone.min.js',
+        'node_modules/systemjs/dist/system.js'
+    ],
+    // pagekage
+    rxjs: 'node_modules/rxjs/**/*'
 };
 
-// clean 任务
-// 清除 wwwroot/scripts/ 目录中的所有文件
-gulp.task('clean_scripts', function () {
-    del(['wwwroot/scripts/**/*']);
+gulp.task('clean_scripts', function (cb) {  // one
+    del(['wwwroot/scripts/**/*']).then(paths => { cb(); });
 });
 
-// clean 任务
-// 清除 wwwroot/lib/node_modules/ 目录中的所有文件
-gulp.task('clean_node_modules', function () {
-    del(['wwwroot/lib/node_modules/**/*']);
+gulp.task('libs', ['clean_scripts'], function (cb) { // two
+    gulp.src(paths.libs).pipe(gulp.dest('wwwroot/scripts/libs')).on('end', cb);
 });
 
-// node_modules 任务
-// 拷贝 node_modules 包
-gulp.task('node_modules', ['clean_node_modules'], function () {
-    gulp.src(paths.node_modules).pipe(gulp.dest('wwwroot/lib/node_modules'));
+gulp.task('rxjs', ['libs'], function (cb) { // three
+    gulp.src(paths.rxjs).pipe(gulp.dest('wwwroot/scripts/libs/')).on('end', cb);
 });
 
-// default 任务
-// 在运行之前，首先执行 lib 任务
-// 将 path.scripts 中的文件复制到 wwwroot/scripts 目录中
-gulp.task('default', ['clean_scripts'], function () {
-    gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/scripts/'));
+gulp.task('scripts', ['rxjs'], function (cb) {  // four
+    gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/scripts')).on('end', cb);
 });
+
+gulp.task('default', ['scripts']);
